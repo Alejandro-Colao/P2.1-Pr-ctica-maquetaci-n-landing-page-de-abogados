beee4c6f260e4120b300cfc32edfe1c2167dd26a
Alejandro-Colao <alumno.718218@ies-azarquiel.es>
2025-11-06 12:15:16 +0100

    navbar responsive

---
+++ app.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector(".menuToggle");
+const nav = document.querySelector(".navLinks");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
